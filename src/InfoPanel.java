5310c5f12e3f20597e746a1469ddb33408d9fe9d
Enzo Quirici <enzomatteo.quirici@formation.orif.ch>
2024-11-04 11:57:57 +0100

    Quick Changes

--- src/InfoPanel.java
+++ src/InfoPanel.java
@@ -1,10 +1,9 @@
 // InfoPanel.java
 
+import platform.LinuxOSInfo;
+
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class InfoPanel {
